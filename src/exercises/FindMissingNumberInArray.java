98a7430d638a2f760dcff52a160fb70262241c95
Max <sveramaxim@gmail.com>
2021-06-05 10:06:14 -0500

    Missing Nr in Array

---
+++ src/exercises/FindMissingNumberInArray.java
@@ -0,0 +1,26 @@
+package exercises;
+
+public class FindMissingNumberInArray {
+    public static void main(String[] args) {
+        //Should not contains any duplicates
+//        int[] a = {4, 3, 1, 5}; // 1+3+4+5
+//        Arrays.sort(a);
+//        int range = a[a.length - 1];
+//        // System.out.println(Arrays.toString(a));
+//        int sum = 0; // 0+1+2+3+4+5
+//        for (int i = 0; i <= range; i++) {
+//            sum += i;
+//        }
+//        System.out.println(sum);
+//        int sumA = 0;
+//        for (int j = 0; j < a.length; j++) {
+//            sumA += a[j];
+//        }
+//        System.out.println(sum-sumA);
+
+
+        // find all missing numbers from 1 to 50
+
+
+    }
+}
