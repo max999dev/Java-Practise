8fb9c53ecdef76f8cb92a2e7fbe98db609942551
Max <sveramaxim@gmail.com>
2021-06-06 17:23:58 -0500

    String manipulations

---
+++ src/exercises/StringManipulation.java
@@ -0,0 +1,18 @@
+package exercises;
+
+public class StringManipulation {
+    public static void main(String[] args) {
+        // String manipulation
+
+        String str = "This string is used for a practise purpose";
+
+        System.out.println(str.length());// length
+        System.out.println(str.charAt(7));
+        System.out.println(str.startsWith("Th"));
+        System.out.println(str.endsWith("sl"));
+        System.out.println(str.indexOf('s'));
+
+
+
+    }
+}
