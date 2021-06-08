4390eee9b6eaf62ec4d0eaeb1e6829cc71343a05
Max <sveramaxim@gmail.com>
2021-06-08 09:33:18 -0500

    Second smalest nr in array

---
+++ src/arrays/SecondSmallestEl.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class SecondSmallestEl {
+    public static void main(String[] args) {
+        int[] numbers = {5, 4, 8, 9, 6, 1}; // need to find 4
+
+        int smallest = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < smallest) {
+                second = smallest;
+                smallest = numbers[i];
+
+            }
+            if (numbers[i] != smallest && numbers[i] > second) {
+
+            }
+        }
+        System.out.println(second);
+    }
+}
