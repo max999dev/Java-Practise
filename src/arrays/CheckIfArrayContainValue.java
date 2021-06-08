8d823d2f1fb0fa4818b5d649bd2693339c256c06
Max <sveramaxim@gmail.com>
2021-06-08 09:31:50 -0500

    Check for the value in array

---
+++ src/arrays/CheckIfArrayContainValue.java
@@ -0,0 +1,27 @@
+package arrays;
+
+public class CheckIfArrayContainValue {
+    public static void main(String[] args) {
+        int[] my_array1 = {
+                1789, 2035, 1899, 1456, 2013,
+                1458, 2458, 1254, 1472, 2365,
+                1456, 2165, 1457, 2456};
+        int value = 1458;
+        int index = 0;
+        for (int i = 0; i < my_array1.length; i++) {
+            if (value == my_array1[i]) {
+                index = i;
+            } else {
+                //System.out.println("Nothing found");
+            }
+        }
+        System.out.println("The element " + value + " Is located at index" + index);
+        int b = 5;
+        String a = "abc";
+        for (int i = 0; i <= b; i++) {
+            a += a;
+        }
+        System.out.println(a);
+
+    }
+}
