2c66ed063dd9c8d1c22c0cff99b25230201ea52a
Max <sveramaxim@gmail.com>
2021-06-13 18:18:14 -0500

    alt Pairs

---
+++ src/codingBat/warmup2/altPairs.java
@@ -0,0 +1,13 @@
+package codingBat.warmup2;
+
+public class altPairs {
+    public static void main(String[] args) {
+        //System.out.println(altPairs(""));
+//        String a = "max";
+//        System.out.println(a.substring(0, 2));
+    }
+}
+//    public static String altPairs(String str) {
+//
+//    }
+//    }
