b1d33acbbf4d8fb6e918435160596fb3bcab3b83
Max <sveramaxim@gmail.com>
2021-06-13 18:18:51 -0500

    arrat123

---
+++ src/codingBat/warmup2/array123.java
@@ -0,0 +1,17 @@
+//package codingBat.warmup2;
+//
+//public class array123 {
+//    public static void main(String[] args) {
+//
+//        int[] nums = {1, 1, 2, 3, 1};
+//        for(int i =0;i < nums.length-1; i++){
+//            if(nums.length < 3){
+//                return false;
+//            if(nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//    }
+//}
