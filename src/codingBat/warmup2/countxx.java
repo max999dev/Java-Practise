3feb18a3c0c7dd6c5239aec48cb0f7c4fdd735ae
Max <sveramaxim@gmail.com>
2021-06-14 09:34:34 -0500

    Count XX

---
+++ src/codingBat/warmup2/countxx.java
@@ -0,0 +1,20 @@
+package codingBat.warmup2;
+
+public class countxx {
+    public static void main(String[] args) {
+        System.out.println(countXX("xxxaxx"));
+    }
+
+    static int countXX(String str) {
+
+        int count = 0;
+
+        for(int i =0; i< str.length()-1; i++){
+            if(str.substring(i,i+2).equals("xx")){
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
