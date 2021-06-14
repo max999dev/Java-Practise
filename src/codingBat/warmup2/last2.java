e4ef974100f3d4c4e888a78e4ae8d4ad7000277c
Max <sveramaxim@gmail.com>
2021-06-14 09:37:09 -0500

    last 2

---
+++ src/codingBat/warmup2/last2.java
@@ -0,0 +1,11 @@
+//package codingBat.warmup2;
+//
+//public class last2 {
+//    public static void main(String[] args) {
+//        System.out.println(last2());
+//    }
+//
+//    public static int last2(String str) {
+//
+//    }
+//}
