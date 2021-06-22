a5e8840489f30e3130650582c34274da8e735c09
Max <sveramaxim@gmail.com>
2021-06-21 20:08:34 -0500

    fizz String

---
+++ src/codingBat/logic1/fizzString.java
@@ -0,0 +1,15 @@
+package codingBat.logic1;
+
+public class fizzString {
+    public String fizzString(String str) {
+        if (str.charAt(0) == 'f' && str.charAt(str.length() - 1) == 'b') {
+            return "FizzBuzz";
+        } else if (str.charAt(0) == 'f') {
+            return "Fizz";
+        } else if (str.charAt(str.length() - 1) == 'b') {
+            return "Buzz";
+        } else {
+            return str;
+        }
+    }
+}
