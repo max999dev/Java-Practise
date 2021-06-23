77e0cbe51c7b59657fe1e259c3c7c7f8a77d606a
Max <sveramaxim@gmail.com>
2021-06-22 20:58:27 -0500

    Less 20

---
+++ src/codingBat/logic1/less20.java
@@ -0,0 +1,11 @@
+package codingBat.logic1;
+
+public class less20 {
+    public static boolean less20(int n) {
+
+        return 20 - (n % 20) == 1 || 20 - (n % 20) == 2;
+    }
+    public static void main(String[] args) {
+
+    }
+}
