1dd590c2b7b17b0e80ac4e1e252c4883e7d7fe2c
Max <sveramaxim@gmail.com>
2021-06-26 08:45:13 -0500

    Bigger Two

---
+++ src/codingBat/array1/biggerTwo.java
@@ -0,0 +1,21 @@
+package codingBat.array1;
+
+import java.util.Arrays;
+
+public class biggerTwo {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new int[]{}));
+    }
+
+    public int[] biggerTwo(int[] a, int[] b) {
+        int sumA = a[0] + a[1];
+        int sumB = b[0] + b[1];
+        if (sumA > sumB){
+            return a;
+        }else if(sumB > sumA){
+            return b;
+        }else{
+            return a;
+        }
+    }
+}
