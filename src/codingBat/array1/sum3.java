e74452831701f422bd84ad78f4abf4fc97430ec3
Max <sveramaxim@gmail.com>
2021-06-27 07:57:35 -0500

    Sum of 3

---
+++ src/codingBat/array1/sum3.java
@@ -0,0 +1,11 @@
+package codingBat.array1;
+
+public class sum3 {
+    public static void main(String[] args) {
+        System.out.println(sum3(new int[]{}));
+    }
+
+    private static int sum3(int[] nums) {
+        return nums[0] + nums[1] + nums[2];
+    }
+}
