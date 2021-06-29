c0fef8260fb9331ba0e3c4cf3ebfa5791470b110
Max <sveramaxim@gmail.com>
2021-06-28 19:08:23 -0500

    Start 1

---
+++ src/codingBat/array1/start1.java
@@ -0,0 +1,23 @@
+//package codingBat.array1;
+//
+//public class start1 {
+//    public static void main(String[] args) {
+//        System.out.println(start1(new int[]{7}, new int[]{8}));
+//
+//    }
+//
+//    private static int start1(int[] a, int[] b) {
+//
+////
+////        if (a.length != 0 && b.length != 0 && a[0] == 1 && b[0] == 1) {
+////            return 2;
+////        } else if (a.length != 0 && b.length != 0 && a[0] == 1) {
+////            return 1;
+////        } else if (a.length != 0 && b.length != 0 && b[0] == 1){
+////            return 1;
+////        }else if(a.length == 0 && b[0] ==1){
+////
+////        }
+////    }
+//
+//}
