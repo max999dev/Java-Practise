e541a015beaa1ea5e7de08fcf4ebfbf90935f5ce
Max <sveramaxim@gmail.com>
2021-07-03 16:31:30 -0500

    Make Pi

---
+++ src/codingBat/array1/makePi.java
@@ -0,0 +1,14 @@
+package codingBat.array1;
+
+import java.util.Arrays;
+
+public class makePi {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new int [] {}));
+    }
+    private static int[] makePi() {
+        int[] arr = {3, 1, 4};
+        return arr;
+    }
+
+    }
