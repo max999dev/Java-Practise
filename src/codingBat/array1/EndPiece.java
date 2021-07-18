45f6cd952e8a18d2deeba89466a2362e351c8d1d
Max <sveramaxim@gmail.com>
2021-07-18 12:01:19 -0500

    End Piece

---
+++ src/codingBat/array1/EndPiece.java
@@ -0,0 +1,19 @@
+package codingBat.array1;
+
+import java.util.Arrays;
+
+public class EndPiece {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(frontPiece(new int []{})));
+    }
+    private static int[] frontPiece(int[] nums) {
+        int [] arr = new int[2];
+        if(nums.length < 2){
+            return nums;
+        }else{
+            arr[0] = nums[0];
+            arr[1] = nums[1];
+            return arr;
+        }
+    }
+}
